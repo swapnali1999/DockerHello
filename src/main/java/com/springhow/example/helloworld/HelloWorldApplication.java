b1f86e9eee3403176595df13cc442b15c6376b4c
swapnali1999 <swapnalisatpute9@gmail.com>
2024-07-04 12:10:36 +0530

    Update HelloWorld

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello swap";
+        return "Hello....";
     }
 
 }
