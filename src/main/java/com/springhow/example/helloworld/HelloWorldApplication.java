3fe9a4d1ad5305dd647167960ee824a278d3a92b
swapnali1999 <swapnalisatpute9@gmail.com>
2024-07-05 16:39:50 +0530

    message changed

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello..Good Morning";
+        return "Hello..Good Morning,";
     }
 
 }
